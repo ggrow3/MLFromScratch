5f8b628799ec43b2fc8310f2f22ab762c4fcb88e
kmacneel <kevjac@gmail.com>
2016-12-05 14:18:34 -0800

    Initial Commit

---
+++ MLFromScratch/Test_LinearRegression.py
@@ -0,0 +1,10 @@
+import unittest
+
+sample_set = [[1,1],[2,3],[4,3],[3,2],[5,5]]
+
+class Test_Test_LinearRegression(unittest.TestCase):
+    def test_A(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
