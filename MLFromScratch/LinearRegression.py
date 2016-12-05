0909b2fe80aaaf6d7761db1693200994462070f4
kmacneel <kevjac@gmail.com>
2016-12-05 14:39:45 -0800

    Added covariance

--- MLFromScratch/LinearRegression.py
+++ MLFromScratch/LinearRegression.py
@@ -24,3 +24,16 @@ def covariance(x, mean_x, y, mean_y):
     for i in range(len(x)):
         covar += (x[i] - mean_x) * (y[i] - mean_y)
 
+covar = covariance(x, mean_x, y,mean_y)
+print('Covariance: %.3f' % (covar))
+
+def coefficients(dataset):
+    x = [row[0] for row in dataset]
+    y = [row[1] for row in dataset]
+    x_mean,y_mean = mean(x), mean(y)
+    b1 = covariance(x,x_mean,y,y_mean)/variance(x,x_mean)
+    b0 = y_mean - b1 * x_mean
+    return [b0,b1]
+
+b0,b1 = coefficients(dataset)
+print('Coefficients: B0%.3f, B1=%.3f' % (b0,b1))
